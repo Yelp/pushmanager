dd5b97fc34773e7d27eff89090559a2051c43623
milki <milki@yelp.com>
2013-06-20 15:47:22 -0700

    refactor test_template_request

--- tests/test_template_request.py
+++ tests/test_template_request.py
@@ -6,13 +6,10 @@ class RequestTemplateTest(T.TemplateTestCase):
     request_page = 'modules/request.html'
     request_info_page = 'modules/request-info.html'
 
-    def render_module_request_with_users(self, request_user, current_user, pushmaster=False):
-        """Provide enough information to render modules/request"""
-        request = {
+    basic_request = {
             'id': 0,
             'repo': 'non-existent',
             'branch': 'non-existent',
-            'user': request_user,
             'reviewid': 0,
             'title': 'some title',
             'revision': '0' * 40,
@@ -21,34 +18,53 @@ class RequestTemplateTest(T.TemplateTestCase):
             'modified': 'nodate',
             'description': 'nondescript',
             'comments': 'nocomment',
-            'watchers': 'watcher1, watcher2',
-        }
+            'watchers': None,
+            }
 
-        web_hooks = {
-            'service_name': 'noname',
-            'get_request_url': 'non://existent',
-        }
+    basic_kwargs = {
+            'pushmaster': False,
+            'web_hooks': {
+                'service_name': 'noname',
+                'get_request_url': 'non://existent',
+                },
+            'cherry_string': '',
+            'expand': False,
+            'push_buttons': False,
+            'edit_buttons': False,
+            'show_ago': False,
+            'tags': None,
+            'show_state_inline': False,
+            'review': None,
+            'repo_url': 'non://existent',
+            'branch_url': 'non://existent',
+            'create_time': 'nodate'
+            }
+
+
+    def render_module_request_with_users(self, request, request_user, current_user, **kwargs):
+        """Provide enough information to render modules/request"""
+        request['user'] = request_user
 
         return self.render_etree(self.request_page,
-            pushmaster=pushmaster,
             current_user=current_user,
             request=request,
-            web_hooks=web_hooks,
-            cherry_string='',
-            expand=False,
-            push_buttons=True,
-            edit_buttons=False,
-            show_ago=False,
-            tags=None,
-            show_state_inline=False,
-            review=None,
-            repo_url='non://existent',
-            branch_url='non://existent',
-            create_time='nodate',
+            **kwargs
         )
 
+    def assert_button_link(self, button, tree, text=None, num=1):
+        classname = '%s-request' % button.lower()
+        if text is None:
+            text = button.capitalize()
+
+        buttons = []
+        for button in tree.iter('button'):
+            if button.attrib['class'] == classname:
+                T.assert_equal(text, button.text)
+                buttons.append(button)
+        T.assert_equal(num, len(buttons))
+
     def test_include_request_info(self):
-        tree = self.render_module_request_with_users('testuser', 'testuser')
+        tree = self.render_module_request_with_users(self.basic_request,'testuser', 'testuser', **self.basic_kwargs)
 
         found_ul = []
         for ul in tree.iter('ul'):
@@ -58,21 +74,8 @@ class RequestTemplateTest(T.TemplateTestCase):
         T.assert_equal(1, len(found_ul))
 
     def test_request_info_user_title(self):
-        request = {
-            'id': 0,
-            'repo': 'non-existent',
-            'branch': 'non-existent',
-            'user': 'testuser',
-            'reviewid': 0,
-            'title': 'some title',
-            'revision': '0' * 40,
-            'state': 'requested',
-            'created': 'nodate',
-            'modified': 'nodate',
-            'description': 'nondescript',
-            'comments': 'nocomment',
-            'watchers': 'watcher1, watcher2',
-        }
+        request = dict(self.basic_request)
+        request['watchers'] = 'watcher1, watcher2'
 
         tree = self.render_etree(self.request_info_page,
             request=request,
