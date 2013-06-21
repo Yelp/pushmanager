d64be03fb47f4349e6da7221fa14c73507896483
milki <milki@yelp.com>
2013-06-20 18:06:13 -0700

    Breakout request buttons for testing
    Additionally protect push buttons from random users

--- tests/test_template_request.py
+++ tests/test_template_request.py
@@ -89,6 +89,111 @@ class RequestTemplateTest(T.TemplateTestCase):
 
         T.assert_equal(title, '%s (%s)' % (request['user'], request['watchers']))
 
+    def test_include_no_request_buttons(self):
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'testuser', **self.basic_kwargs)
+
+        for span in tree.iter('span'):
+            T.assert_not_equal('push-request-buttons', span.attrib['class'])
+            T.assert_not_equal('edit-request-buttons', span.attrib['class'])
+
+    def test_include_push_buttons(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['push_buttons'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'testuser', **kwargs)
+
+        found_span = []
+        for span in tree.iter('span'):
+            T.assert_not_equal('edit-request-buttons', span.attrib['class'])
+            if span.attrib['class'] == 'push-request-buttons':
+                found_span.append(span)
+
+        T.assert_equal(1, len(found_span))
+
+    def test_include_edit_buttons(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['edit_buttons'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'testuser', **kwargs)
+
+        found_span = []
+        for span in tree.iter('span'):
+            T.assert_not_equal('push-request-buttons', span.attrib['class'])
+            if span.attrib['class'] == 'edit-request-buttons':
+                found_span.append(span)
+
+        T.assert_equal(1, len(found_span))
+
+    pushmaster_button_classes = ['add-request', 'remove-request', 'comment-request', 'pushmaster-delay-request']
+    pushmaster_button_text = ['Add', 'Remove', 'Comment', 'Delay']
+    push_button_classes = ['verify-request', 'pickme-request', 'unpickme-request']
+    push_button_text = ['Verify', 'Pick me!', 'Don\'t pick me!']
+    requester_edit_button_classes = ['edit-request', 'delay-request', 'discard-request']
+    requester_edit_button_text = ['Edit', 'Delay', 'Discard']
+    edit_button_classes = ['edit-request']
+    edit_button_text = ['Edit']
+
+    def assert_request_buttons(self, tree, button_classes, button_text):
+        found_buttons = []
+        for button in tree.iter('button'):
+            T.assert_in(button.attrib['class'], button_classes)
+            T.assert_in(button.text, button_text)
+            found_buttons.append(button)
+        T.assert_equal(len(button_classes), len(found_buttons))
+
+    def test_request_push_buttons_as_random_user(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['push_buttons'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'notuser', **kwargs)
+
+        buttons_found = []
+        for button in tree.iter('button'):
+            T.assert_not_in(button.attrib['class'], self.pushmaster_button_classes + self.push_button_classes)
+            buttons_found.append(button)
+
+        T.assert_equal(0, len(buttons_found))
+
+    def test_request_push_buttons_as_request_user(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['push_buttons'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'testuser', **kwargs)
+        self.assert_request_buttons(tree, self.push_button_classes, self.push_button_text)
+
+    def test_request_push_buttons_as_pushmaster(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['push_buttons'] = True
+        kwargs['pushmaster'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'notuser', **kwargs)
+        self.assert_request_buttons(
+            tree,
+            self.pushmaster_button_classes + self.push_button_classes,
+            self.pushmaster_button_text + self.push_button_text)
+
+    def test_request_edit_buttons_as_random_user(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['edit_buttons'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'notuser', **kwargs)
+        self.assert_request_buttons(tree, self.edit_button_classes, self.edit_button_text)
+
+    def test_request_edit_buttons_as_request_user(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['edit_buttons'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'testuser', **kwargs)
+        self.assert_request_buttons(tree, self.requester_edit_button_classes, self.requester_edit_button_text)
+
+    def test_request_edit_buttons_as_pushmaster(self):
+        kwargs = dict(self.basic_kwargs)
+        kwargs['edit_buttons'] = True
+        kwargs['pushmaster'] = True
+
+        tree = self.render_module_request_with_users(self.basic_request, 'testuser', 'notuser', **kwargs)
+        self.assert_request_buttons(tree, self.edit_button_classes, self.edit_button_text)
+
 
 if __name__ == '__main__':
     T.run()
