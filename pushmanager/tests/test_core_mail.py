6ff21526c611597521cb448c9dd4b7ab5d908646
milki <milki@yelp.com>
2015-01-27 13:04:13 -0800

    Introduce mail.notifyonly
    mail.notifyonly overrides all To addressing and redirects the mail to
    the addresses in mail.notifyonly.
    
    The original recipients of the mail are preserved by prepending the
    addresses in the email body.

--- pushmanager/tests/test_core_mail.py
+++ pushmanager/tests/test_core_mail.py
@@ -1,18 +1,46 @@
+import copy
 import mock
-import pushmanager.core.mail
 import testify as T
+
+from pushmanager.core.settings import Settings
+import pushmanager.core.mail
 
 
 class MailQueueTest(T.TestCase):
 
-    def test_send_mail(self):
+    @T.setup_teardown
+    def mock_smtp(self):
         with mock.patch.object(pushmanager.core.mail.MailQueue, "smtp") as mocked_smtp:
             with mock.patch.object(pushmanager.core.mail.MailQueue, "message_queue"):
-                recipient = "test@test.com"
-                message = "test message"
-                subject = "test subject"
-                from_email = "fromtest@test.com"
+                self.mocked_smtp = mocked_smtp
+                self.MockedSettings = copy.deepcopy(Settings)
+                yield
 
-                pushmanager.core.mail.MailQueue._send_email(recipient, message, subject, from_email)
+    def test_send_mail(self):
+        recipient = "test@test.com"
+        message = "test message"
+        subject = "test subject"
+        from_email = "fromtest@test.com"
 
-                T.assert_equal(mocked_smtp.sendmail.called, True)
+        pushmanager.core.mail.MailQueue._send_email(recipient, message, subject, from_email)
+
+        T.assert_equal(self.mocked_smtp.sendmail.called, True)
+        self.mocked_smtp.sendmail.assert_any_call(from_email, [recipient], mock.ANY)
+        self.mocked_smtp.sendmail.assert_any_call(from_email, self.MockedSettings['mail']['notifyall'], mock.ANY)
+
+    def test_mail_notifyonly(self):
+        self.MockedSettings['mail']['notifyonly'] = [ 'notifyme', 'notifyyou', 'notifyhim', 'notifyher' ]
+        with mock.patch.dict(Settings, self.MockedSettings):
+            recipient = "test@test.com"
+            message = "test message"
+            subject = "test subject"
+            from_email = "fromtest@test.com"
+
+            pushmanager.core.mail.MailQueue._send_email(recipient, message, subject, from_email)
+
+            T.assert_equal(self.mocked_smtp.sendmail.called, True)
+            self.mocked_smtp.sendmail.assert_called_once_with(from_email, self.MockedSettings['mail']['notifyonly'], mock.ANY)
+
+            args = self.mocked_smtp.sendmail.call_args_list[0][0]
+            body = "Original recipients: %s\n\n%s" % (recipient, message)
+            T.assert_equal(args[2].endswith(body), True)
