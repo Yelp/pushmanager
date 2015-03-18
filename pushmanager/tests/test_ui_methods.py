e2eae2326c7287e8d91ab68b380b4290866297f3
Qi Qi <qiqi@yelp.com>
2015-03-18 11:59:13 -0700

    Add test for sort_pickmes

--- pushmanager/tests/test_ui_methods.py
+++ pushmanager/tests/test_ui_methods.py
@@ -1,5 +1,6 @@
 import testify as T
 from pushmanager.ui_methods import authorized_to_manage_request
+from pushmanager.ui_methods import sort_pickmes
 
 
 class UIMethodTest(T.TestCase):
@@ -19,6 +20,47 @@ class UIMethodTest(T.TestCase):
     def test_authorized_to_manage_request_watcher(self):
         request = {'user': 'testuser', 'watchers': 'watcher1'}
         T.assert_equal(True, authorized_to_manage_request(None, request, 'watcher1'))
+
+    def test_sort_pickmes_regular_case(self):
+        requests = [
+            {'tags': 'fake_tags'},
+            {'tags': 'urgent,no-verify'},
+            {'tags': 'urgent'},
+            {'tags': 'urgent,no-verify,seagull'},
+        ]
+
+        sorted_requests = sort_pickmes(None, requests, ['urgent', 'no-verify', 'seagull'])
+        T.assert_equal(len(sorted_requests), 4)
+        T.assert_equal(sorted_requests[0], {'tags': 'urgent,no-verify,seagull'})
+        T.assert_equal(sorted_requests[1], {'tags': 'urgent,no-verify'})
+        T.assert_equal(sorted_requests[2], {'tags': 'urgent'})
+        T.assert_equal(sorted_requests[3], {'tags': 'fake_tags'})
+
+    def test_sort_pickmes_all_tags_in_ordering(self):
+        requests = [
+            {'tags': 'no-veryify'},
+            {'tags': 'urgent'},
+            {'tags': 'seagull'},
+        ]
+
+        sorted_requests = sort_pickmes(None, requests, ['urgent', 'no-verify', 'seagull'])
+        T.assert_equal(len(sorted_requests), 3)
+        T.assert_equal(sorted_requests[0], {'tags': 'urgent'})
+        T.assert_equal(sorted_requests[1], {'tags': 'no-verify'})
+        T.assert_equal(sorted_requests[2], {'tags': 'seagull'})
+
+    def test_sort_pickmes_no_tags_order(self):
+        requests = [
+            {'tags': 'no-veryify'},
+            {'tags': 'urgent'},
+            {'tags': 'seagull'},
+        ]
+
+        sorted_requests = sort_pickmes(None, requests, [])
+        T.assert_equal(len(sorted_requests), 3)
+        T.assert_equal(sorted_requests[0], {'tags': 'no-veryify'})
+        T.assert_equal(sorted_requests[1], {'tags': 'urgent'})
+        T.assert_equal(sorted_requests[2], {'tags': 'seagull'})
 
 
 if __name__ == '__main__':
